ebbe74e089efceb38fff80c6595190548e004781
Ari Ehrmann <ari.ehrmann@gmail.com>
2016-04-03 13:06:23 -0500

    Finish initial basic styling
    * No links to projects yet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 ;(function($) {
   $(document).ready(function() {
-    console.log('document ready');
+    $('.project p').hide();
+
+    $('.project h3').on('click', function() {
+      $('.project p').slideUp(300);
+      $(this).next().slideDown(300);
+    });
   });
 })(jQuery);
