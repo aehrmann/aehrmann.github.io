19a34365c5af2e7a61a081350a57ed50b53d2e54
Ari Ehrmann <ari.ehrmann@gmail.com>
2016-04-06 07:41:00 -0500

    Add basic mobile styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
 ;(function($) {
   $(document).ready(function() {
-    $('.project p').hide();
-
-    $('.project h3').on('click', function() {
-      $('.project p').slideUp(300);
-      $(this).next().slideDown(300);
-    });
   });
 })(jQuery);
